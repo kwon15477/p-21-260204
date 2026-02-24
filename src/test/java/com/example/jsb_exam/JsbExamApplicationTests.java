190a5f39c7d3849cdd9f73a47ccde400b160e0e8
kwon15477 <kwon15477@gmail.com>
2026-02-24 15:16:30 +0900

    init

---
+++ src/test/java/com/example/jsb_exam/JsbExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsb_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsbExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
