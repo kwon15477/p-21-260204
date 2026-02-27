0a525f43c9670233daaa76f96738f360af2daffb
kwon15477 <kwon15477@gmail.com>
2026-02-27 10:21:49 +0900

    @ManyToOne

---
+++ src/main/java/com/back/MainController.java
@@ -0,0 +1,21 @@
+package com.back;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
